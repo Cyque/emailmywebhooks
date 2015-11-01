6c97c4c88532a917770cd583282d4b98f4ba207e
Damian <damian.polan@gmail.com>
2015-10-31 20:28:32 -0400

    a

--- routes/actions.js
+++ routes/actions.js
@@ -59,7 +59,7 @@ exports.createWebhook = function(req, res) {
 			},	
 			function (error, response, body) {
 				var bodyP = JSON.parse(body);
-				if (!error || (typeof body[errors] == "undefined")) {
+				if (!error || (typeof body["errors"] == "undefined")) {
 					console.log('Success adding webhook:');
 					console.log(body);
 
