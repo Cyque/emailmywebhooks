d853d351dbb73838ceb69a7d7c8f1cbd9657eba9
Damian <damian.polan@gmail.com>
2015-11-03 11:09:48 -0500

    a

--- routes/hookhandle.js
+++ routes/hookhandle.js
@@ -5,33 +5,42 @@ exports.handleWebhook = function(req, res) {
 
 
 	console.log("RECIEVED A WEBHOOK CALL!!");
-	var transporter = nodemailer.createTransport({
-		service: 'Gmail',
-		auth: {
-			user: 'emailmywebhooks@gmail.com',
-			pass: 'damian10'
-		}
+	// var transporter = nodemailer.createTransport({
+	// 	service: 'Gmail',
+	// 	auth: {
+	// 		user: 'emailmywebhooks@gmail.com',
+	// 		pass: 'damian10'
+	// 	}
+	// });
+
+	// // NB! No need to recreate the transporter object. You can use
+	// // the same transporter object for all e-mails
+
+	// // setup e-mail data with unicode symbols
+	// var mailOptions = {
+	//     from: 'EmailMyWebhooks ✔ <emailmywebhooks@gmail.com>', // sender address
+	//     to: 'damian.polan@gmail.com', // list of receivers
+	//     subject: 'Hello ✔', // Subject line
+	//     text: 'Hello world ✔', // plaintext body
+	//     html: '<b>Hello world ✔</b>' // html body
+	// };
+
+	// // send mail with defined transport object
+	// transporter.sendMail(mailOptions, function(error, info){
+	// 	if(error){
+	// 		return console.log(error);
+	// 	}
+	// 	console.log('Message sent: ' + info.response);
+
+	// });
+
+	var transporter = nodemailer.createTransport();
+	transporter.sendMail({
+		from: 'sender@address',
+		to: 'damian.polan@gmail.com',
+		subject: 'hello',
+		text: 'hello world!'
 	});
 
-	// NB! No need to recreate the transporter object. You can use
-	// the same transporter object for all e-mails
-
-	// setup e-mail data with unicode symbols
-	var mailOptions = {
-	    from: 'EmailMyWebhooks ✔ <emailmywebhooks@gmail.com>', // sender address
-	    to: 'damian.polan@gmail.com', // list of receivers
-	    subject: 'Hello ✔', // Subject line
-	    text: 'Hello world ✔', // plaintext body
-	    html: '<b>Hello world ✔</b>' // html body
-	};
-
-	// send mail with defined transport object
-	transporter.sendMail(mailOptions, function(error, info){
-		if(error){
-			return console.log(error);
-		}
-		console.log('Message sent: ' + info.response);
-
-	});
-	res.send("Not implemented.");
+res.send("Not implemented.");
 }
