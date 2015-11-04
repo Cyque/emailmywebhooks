bb292de47b7e4b4c6f23b3f93628482c896900af
Damian <damian.polan@gmail.com>
2015-11-04 12:37:42 -0500

    a

--- routes/hookhandle.js
+++ routes/hookhandle.js
@@ -6,16 +6,18 @@ var transporter = nodemailer.createTransport();
 exports.handleWebhook = function(req, res) {
 	console.log("RECIEVED A WEBHOOK CALL!!");
 
-
-	console.log(req.body);
-
-
 	transporter.sendMail({
 		from: 'emailmywebhooks@noreply',
 		to: 'damian.polan@gmail.com',
 		subject: 'WEBHOOK',
 		text: JSON.stringify(req.body)
+	},  
+	function(error, info) {
+		if(error){
+			return console.log(error);
+		}
+		
+		res.sendStatus(200);
 	});
 
-	res.sendStatus(200);
 }
