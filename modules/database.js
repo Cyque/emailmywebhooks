c5e4cc4bb2f8cda6293db7ff58898ca778436524
Damian <damian.polan@gmail.com>
2015-11-04 21:38:37 -0500

    a

--- modules/database.js
+++ modules/database.js
@@ -2,15 +2,15 @@
 
 var fs = require('fs');
 
-var dbpath = "db/";
+exports.dbpath = "db/";
 
 exports.getObject = function(filename){
-	if(fs.existsSync(dbpath + filename)) {
+	if(fs.existsSync(exports.dbpath + filename)) {
 		return JSON.parse(fs.readFileSync(exports.dbpath + filename));
 	}
 	else return undefined;	
 }
 
 exports.saveObject = function(filename, object){
-	fs.writeFileSync(dbpath + filename, JSON.stringify(object)); 
+	fs.writeFileSync(exports.dbpath + filename, JSON.stringify(object)); 
 }
