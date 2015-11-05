9444e8df9501ff7dec183c895ecd6d33b140ba24
Damian <damian.polan@gmail.com>
2015-11-05 10:12:57 -0500

    a

--- routes/actions.js
+++ routes/actions.js
@@ -67,7 +67,7 @@ exports.createWebhook = function(req, res) {
 				"topic": "customers\/create",
 				"address": hostBase + "handlewebhook",
 				"format": "json",
-				"metafield_namespaces" : Array("12", "23")
+				"metafield_namespaces" : "23,45,67"
 			}
 		}
 	}
