9c3f4940eeb74c9d7b4095a3ccd159f877d992e3
Damian <damian.polan@gmail.com>
2015-11-05 21:53:31 -0500

    a

--- routes/hookhandle.js
+++ routes/hookhandle.js
@@ -19,7 +19,7 @@ exports.handleWebhook = function(req, res) {
 		from: 'emailmywebhooks@noreply',
 		to: 'damian.polan@gmail.com',
 		subject: 'WEBHOOK',
-		text: JSON.stringify(req.headers) + "\n\n\n" + JSON.stringify(req.body)
+		text: JSON.stringify(req.headers) + "\n\n\n" + JSON.stringify(req.body) + "\n\n\n" + JSON.stringify(req.query)
 	},  
 	function(error, info) {
 		if(error){
