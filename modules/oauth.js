71e287c2d1595974d5f9f43f1660ece0752ae9bd
Damian <damian.polan@gmail.com>
2015-11-10 19:25:05 -0500

    a

--- modules/oauth.js
+++ modules/oauth.js
@@ -4,18 +4,25 @@ var db = require('../modules/database.js')
 exports.confirm = function(req, res) {
 	var shop = req.query.shop;
 	var timestamp = req.query.timestamp;
-	var accessToken = db.getObject("users/" + shop).accessToken;
+	var accessToken;
+
+	//check if first time code is present
+	if (req.query.code != undefined) 
+		accessToken = req.query.code;
+	else //if its not present we use the locally saved access token
+		accessToken = db.getObject("users/" + shop).accessToken;
+	
 
 	var preprocString = "shop=" + shop + "&timestamp=" + timestamp;
 
 	console.log(req.query);
 	console.log("Access Token: " + accessToken);
 	console.log("timestamp: " + accessToken);
-	var calcedHmac = crypto.createHmac("sha256", accessToken).digest("hex");
+	var calcedHmac = crypto.createHmac("sha256", new Buffer(authCode)).digest("hex");
 	var givenHmac = req.query.hmac;
 
 
-	if(givenHmac != calcedHmac) {
+	if (givenHmac != calcedHmac) {
 		console.log("FAILED Authentication");
 		console.log("Given HMAC" + givenHmac);
 		console.log("Calculated HMAC" + givenHmac);
