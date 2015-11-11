1b6b18a45e4824190de9a505cd447bc9c721a1b6
Damian <damian.polan@gmail.com>
2015-11-11 11:15:31 -0500

    a

--- modules/oauth.js
+++ modules/oauth.js
@@ -4,19 +4,16 @@ var db = require('../modules/database.js')
 exports.confirm = function(query) {
 	var shop = query.shop;
 	var timestamp = query.timestamp;
-	var secret = "1604e972c082a4a3bb6384c1460f3458";
+	var shared_secret = "1604e972c082a4a3bb6384c1460f3458";
 
 	var preprocString = encodeParamsForSignature(query);
 
 	// console.log(query);
-	console.log("Secret: " + secret);
+	console.log("shared_secret: " + shared_secret);
 	console.log("timestamp: " + timestamp);
 	console.log("preprocString " + preprocString);
 
-
-	// var calcedHmac = crypto.createHash("sha256").update(new Buffer(secret)).digest("hex");
-
-	var calcedHmac = crypto.createHmac("SHA256", preprocString).update(new Buffer(secret)).digest('hex');
+	var calcedHmac = crypto.createHmac("SHA256", shared_secret).update(new Buffer(preprocString, 'hex').digest('hex');
 
 	var givenHmac = query.hmac;
 
