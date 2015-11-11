2f7bd424daf3afbd1f963ebaeebc132340198a38
Damian <damian.polan@gmail.com>
2015-11-11 13:43:49 -0500

    a

--- modules/oauth.js
+++ modules/oauth.js
@@ -25,15 +25,13 @@ function isValidShop(query) {
 }
 
 function isValidHmac(query) {
-	//var shop = query.shop;
-	//var timestamp = query.timestamp;
 	var shared_secret = "1604e972c082a4a3bb6384c1460f3458";
 
 	var preprocString = encodeParamsForSignature(query);
 
 	// console.log(query);
 	console.log("shared_secret: " + shared_secret);
-	console.log("timestamp: " + timestamp);
+	console.log("timestamp: " + query.query.timestamp);
 	console.log("preprocString " + preprocString);
 
 	var calcedHmac = crypto.createHmac("SHA256", shared_secret).update(new Buffer(preprocString)).digest('hex');
@@ -52,13 +50,9 @@ function isValidHmac(query) {
 
 
 function encodeParamsForSignature(object) {
-	// var list = ["protocol=https://"];
 	var list = [];
 	for (var property in object) {
 		if (object.hasOwnProperty(property)) {
-			// if(property == "shop" || property == "timestamp")
-			// 	list.push(property + "=" + object[property]);
-
 			if (property != "hmac" && property != "signature")
 				list.push(property + "=" + object[property]);
 		}
