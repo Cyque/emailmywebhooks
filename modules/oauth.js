90c12224c5f97510f94796b97225b340ed543126
Damian <damian.polan@gmail.com>
2015-11-10 19:35:29 -0500

    a

--- modules/oauth.js
+++ modules/oauth.js
@@ -28,9 +28,12 @@ exports.confirm = function(req, res) {
 	if (givenHmac != calcedHmac) {
 		console.log("FAILED Authentication");
 		console.log("Given HMAC      " + givenHmac);
-		console.log("Calculated HMAC " + givenHmac);
+		console.log("Calculated HMAC " + calcedHmac);
 		res.send("Failed Authentication.");
 		return false;
 	}
 	return true;
 }
+
+93087798a68d44109e1751a4fbeda0578fdfda962eecc1703602481bd5cd42ca
+93087798a68d44109e1751a4fbeda0578fdfda962eecc1703602481bd5cd42ca
