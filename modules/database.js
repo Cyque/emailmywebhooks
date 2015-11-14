6cb7e97975a67fea1a8dfddac5c04eaf963f2ac6
Damian <damian.polan@gmail.com>
2015-11-14 17:37:53 -0500

    a

--- modules/database.js
+++ modules/database.js
@@ -1,5 +1,3 @@
-
-
 /*
 
 FORMAT webhook:
@@ -77,48 +75,42 @@ filename = damiansstoree.myshopify.com (shop name)
 
 
 
-
-
-
 var fs = require('fs');
 var pg = require('pg');
 
 // process.env['DATABASE_URL'] = 'postgres://rrgnptrdfrxley:OPtMCtDlA1uoqyZ6-rrJyLihi6@ec2-107-21-223-147.compute-1.amazonaws.com:5432/d1frkvg5roavqs';
 exports.dbpath = "db/"
 
-exports.getObject = function(filename){
+exports.getObject = function(filename) {
 	// var split = filename.split('/');
 	// var table = split[0];
 	// var key = split[1];
 
-	// pg.connect(process.env.DATABASE_URL, function(err, client) {
-	// 	if (err) throw err;
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+		if (err) throw err;
 
-	// 	console.log('Connected to postgres! Getting schemas...');
+		console.log('Connected to postgres!');
 
-	// 	client.query("CREATE TABLE objects(filename char(100) PRIMARY KEY NOT NULL, DATA  CHAR(5000) NOT NULL);");
-
-
-	// 	// client.query('SELECT table_schema,table_name FROM information_schema.tables;').on('row', function(row) {
-	// 	// 	console.log(JSON.stringify(row));
-	// 	// });
-	// });
+		// client.query("CREATE TABLE objects(filename char(100) PRIMARY KEY NOT NULL, DATA  CHAR(5000) NOT NULL);");
 
 
-if(fs.existsSync(exports.dbpath + filename)) {
-	return JSON.parse(fs.readFileSync(exports.dbpath + filename));
-}
-else {
-	console.log("ERROR: FILE " + filename + " NOT FOUND.")
-	return undefined;	
-}
-}
+		// client.query('SELECT table_schema,table_name FROM information_schema.tables;').on('row', function(row) {
+		// 	console.log(JSON.stringify(row));
+		// });
+	});
 
-exports.saveObject = function(filename, object){
-	fs.writeFileSync(exports.dbpath + filename, JSON.stringify(object)); 
+
+	if (fs.existsSync(exports.dbpath + filename)) {
+		return JSON.parse(fs.readFileSync(exports.dbpath + filename));
+	} else {
+		console.log("ERROR: FILE " + filename + " NOT FOUND.")
+		return undefined;
+	}
 }
 
-
+exports.saveObject = function(filename, object) {
+	fs.writeFileSync(exports.dbpath + filename, JSON.stringify(object));
+}
 
 
 
