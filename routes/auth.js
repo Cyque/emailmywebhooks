a75701032052d209a0c8d10f5223311e7ffff6a1
Damian <damian.polan@gmail.com>
2015-11-16 15:19:40 -0500

    a

--- routes/auth.js
+++ routes/auth.js
@@ -108,8 +108,8 @@ exports.confirm = function(req, res) {
 								if (!error && response.statusCode == 200) {
 									addShopInfoFor(shop, JSON.parse(body).shop);
 
-									res.cookie('GLOB_API_KEY', api_key, { secure: true });
-									res.cookie('GLOB_SHOP', shop, { secure: true });
+									res.cookie('GLOB_API_KEY', api_key);
+									res.cookie('GLOB_SHOP', shop);
 									// res.cookie('shopName', shop, { httpOnly: true });
 									//FULLY AUTHENTICATED HERE
 									res.redirect('home');
