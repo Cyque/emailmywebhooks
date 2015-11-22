8bdfe2a4d5865229f3cf2db1f2150c30662ea5b5
Damian <damian.polan@gmail.com>
2015-11-22 16:27:18 -0500

    a

--- public/html/home.js
+++ public/html/home.js
@@ -6,7 +6,24 @@ ShopifyApp.init({
 });
 
 ShopifyApp.ready(function(){
+	console.log("READY");
   	ShopifyApp.Bar.loadingOff()
-
 });
 
+
+function toggleWebhook(checked, webhookName) {
+
+	if (checked) {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				console.log("Success");
+			}
+		};
+		xhttp.open("GET", "action/createhook", true);
+		xhttp.send();
+	} else {
+
+
+	}
+}
