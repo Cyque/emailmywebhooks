b1b2e08cdce776cc4518f36f0fff69295e7702ad
Damian <damian.polan@gmail.com>
2015-11-22 21:17:22 -0500

    a

--- routes/permission.js
+++ routes/permission.js
@@ -8,9 +8,8 @@ var oauth = require('../modules/oauth.js')
 
 //called by the shopify API.
 exports.confirm = function(req, res) {
-
-	//WILL RECIEVE THIS
-	// http://localhost:3000/auth/confirm?
+	//WILL RECIEVE THIS Format:
+	// /auth/confirm?
 	// code=6860806f796c3eb7fce89128cfb067cb&
 	// hmac=762228afe84973729eacc11d7e5ebcc7aa917575bebc104a4865ccf39b5cefac&
 	// shop=damianpolan.myshopify.com&
@@ -18,41 +17,72 @@ exports.confirm = function(req, res) {
 	// state=6d256e1719ade112a00711e5d64de6e0&\
 	// timestamp=1446246040
 
-	// console.log("FULL CONFIRM URL:" + req.originalUrl);
-	// console.log(req.query);
+	function getAccessToken(req, res, shopObject, callback) {
+		var shop = req.query.shop;
+		var code = req.query.code;
+
+		request.post("https://" + shop + "/admin/oauth/access_token", {
+			form: {
+				client_id: process.env.api_key,
+				client_secret: process.env.shared_secret,
+				code: code
+			}
+		}, function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				//body format: {"access_token":"xxxxx ... xxxx"}
+				var accTok = JSON.parse(body).access_token;
+				addAccessTokenFor(shop, accTok);
+				callback(accTok);
+			} else {
+				res.status(401).send("There was a problem getting the access token.");
+			}
+		});
+	}
+
 
 	oauth.verifyRequest(req, res, function() {
 		var shop = req.query.shop;
-		var code = req.query.code;
-		var token = req.query.state;
-
 		db.getShop(shop, function(shopObject) {
-			request.post("https://" + shop + "/admin/oauth/access_token", {
-				form: {
-					client_id: process.env.api_key,
-					client_secret: process.env.shared_secret,
-					code: code,
-				}
-			}, function(error, response, body) {
-				if (!error && response.statusCode == 200) {
-					//body format: {"access_token":"xxxxx ... xxxx"}
+			//verify nonce
+			var nonce = req.query.state;
+			if (nonce == shopObject.nonce) {
+				//get access token
+				getAccessToken(req, res, shopObject, function(accessToken) {
+					//get shop info
+					request.get(
+						"https://" + shop + "/admin/shop.json", {
+							auth: {
+								user: process.env.api_key,
+								pass: process.env.shared_secret
+							},
+							headers: {
+								'X-Shopify-Access-Token': accessToken
+							}
+						},
+						function(error, response, body) {
+							if (!error && response.statusCode == 200) {
+								var bodyP = JSON.parse(body);
+								addShopInfoFor(shop, bodyP.shop);
 
-					var accTok = JSON.parse(body).access_token;
-					addAccessTokenFor(shop, accTok);
-
-					//we encode the hmac here for the redirect back to the home page
-					res.redirect("/home?shop=" + encodeURIComponent(shop) + "&hmac=" + oauth.generateHmac({
-						shop: shop
-					}));
-				} else {
-					res.status(401).send("There was a problem getting the access token.");
-				}
-			});
+								//we encode the hmac here for the redirect back to the home page
+								res.redirect("/home?shop=" + encodeURIComponent(shop) + "&hmac=" + oauth.generateHmac({
+									shop: shop
+								}));
+							} else {
+								db.getShop(shop, function(shopObject) {
+									shopObject.accessToken = undefined;
+									db.saveShop(shop, shopObject);
+								}); //delete access token
+								res.status(401).send("Failed to get shopInfo");
+							}
+						});
+				});
+			} else {
+				res.status(401).send("Failed nonce");
+			}
 		});
-
-
 	});
-}
+}; //end confirm
 
 
 exports.verifyPermission = function(req, res, callback) {
